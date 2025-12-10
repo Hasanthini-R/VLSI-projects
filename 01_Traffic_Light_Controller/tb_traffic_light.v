cf0eac7fe56a89142573cb8ead6bc2b3e25b1088
Hasanthini-R <162776484+Hasanthini-R@users.noreply.github.com>
2025-12-10 19:28:39 +0530

    Add testbench for traffic light controller

---
+++ 01_Traffic_Light_Controller/tb_traffic_light.v
@@ -0,0 +1,18 @@
+module tb;
+    reg clk, rst;
+    wire red, yellow, green;
+    traffic_light dut (.clk(clk), .rst(rst), .red(red), .yellow(yellow), .green(green));
+  always #5 clk = ~clk;
+  initial begin
+      clk = 0; 
+      rst = 1; #17 
+      rst = 0; #1000 
+      $finish;
+  end
+  initial begin 
+      $dumpfile("waves/traffic.vcd"); 
+      $dumpvars(0,tb); 
+  end
+  initial 
+      $monitor("%0t state=%0d R=%b Y=%b G=%b", $time, dut.state, red, yellow, green);
+endmodule
