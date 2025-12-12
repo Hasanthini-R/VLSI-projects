6f143c626eeba5252c8156356d58780d68fc7e58
Hasanthini-R <162776484+Hasanthini-R@users.noreply.github.com>
2025-12-12 15:56:06 +0530

    added testbench for synchronous FIFO module

--- 03_Synchronous_FIFO/tb/tb_fifo.v
+++ 03_Synchronous_FIFO/tb/tb_fifo.v
@@ -1,49 +1,39 @@
 module tb;
-  reg clk, rst;
-  reg wr_en, rd_en;
-  reg [7:0] data_in;
-  wire [7:0] data_out;
-  wire full, empty;
+    reg clk, rst, wr_en, rd_en;
+    reg [7:0] data_in;
+    wire [7:0] data_out;
+    wire full, empty, almost_full, almost_empty;
 
-  sync_fifo fifo (.*); 
+    sync_fifo fifo (.*);
 
-  always #5 clk = ~clk; 
+    always #5 clk = ~clk;
 
-  initial begin
-    clk = 0;
-    rst = 1; #20
-    rst = 0;
+    initial begin
+        clk = 0; rst = 1; wr_en = 0; rd_en = 0; data_in = 0;
+        #20 rst = 0;
 
-  repeat(8) begin
-  @(posedge clk); 
-    wr_en = 1;
-    data_in = data_in + 1;
-  end
-  wr_en = 0; 
+        repeat(8) begin
+            @(posedge clk);
+            wr_en = 1; data_in = data_in + 1;
+        end
+        @(posedge clk) wr_en = 0;
 
-  @(posedge clk);
-    wr_en = 1; data_in = 99;
-  @(posedge clk);
-    wr_en = 0;
+        @(posedge clk) wr_en = 1; data_in = 99;
+        @(posedge clk) wr_en = 0;
 
-  repeat(8) begin
-  @(posedge clk);
-    rd_en = 1;
-  end
-  rd_en = 0;
+        repeat(8) begin
+            @(posedge clk);
+            rd_en = 1;
+        end
+        @(posedge clk) rd_en = 0;
 
-  @(posedge clk);
-    rd_en = 1;
-  @(posedge clk);
-    rd_en = 0;
+        @(posedge clk) rd_en = 1;
+        #100 $finish;
+    end
+    initial $monitor("%0t   count=%d   full=%b   empty=%b   data_out=%h", $time, fifo.count, full, empty, data_out);
 
-  #100 $finish; 
-  end
-
-  initial $monitor("Time=%4t items=%d full=%b empty=%b out=%h", $time, fifo.count, full, empty, data_out);
-
-  initial begin
-    $dumpfile("fifo.vcd");
-    $dumpvars(0, tb);
-  end
+    initial begin
+        $dumpfile("fifo.vcd");
+        $dumpvars(0, tb);
+    end
 endmodule
