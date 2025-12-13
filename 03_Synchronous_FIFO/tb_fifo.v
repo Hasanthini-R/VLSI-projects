9a9724a13a67ebc10ba2f61141747dd7fd7bcdca
Hasanthini-R <162776484+Hasanthini-R@users.noreply.github.com>
2025-12-13 09:22:14 +0530

    Refactor testbench for FIFO with new data input

--- 03_Synchronous_FIFO/tb_fifo.v
+++ 03_Synchronous_FIFO/tb_fifo.v
@@ -9,27 +9,32 @@ module tb;
     always #5 clk = ~clk;
 
     initial begin
-        clk = 0; rst = 1; wr_en = 0; rd_en = 0; data_in = 0;
-        #20 rst = 0;
-
-        repeat(8) begin
-            @(posedge clk);
-            wr_en = 1; data_in = data_in + 1;
-        end
-        @(posedge clk) wr_en = 0;
-
-        @(posedge clk) wr_en = 1; data_in = 99;
-        @(posedge clk) wr_en = 0;
-
-        repeat(8) begin
-            @(posedge clk);
-            rd_en = 1;
-        end
-        @(posedge clk) rd_en = 0;
-
-        @(posedge clk) rd_en = 1;
-        #100 $finish;
+    clk = 0; rst = 1; wr_en = 0; rd_en = 0; data_in = 0;
+    #20 rst = 0;
+        
+    repeat(8) begin
+        @(posedge clk);
+        wr_en = 1;
+        data_in = data_in + 1;
     end
+    wr_en = 0;                     
+        
+    @(posedge clk);
+    wr_en = 1; data_in = 8'hAB;
+    wr_en = 0;
+        
+    repeat(8) begin
+        @(posedge clk);
+        rd_en = 1;
+    end
+    rd_en = 0;                  
+        
+    @(posedge clk);
+    rd_en = 1;
+    rd_en = 0;
+
+    #100 $finish;
+end
     initial $monitor("%0t   count=%d   full=%b   empty=%b   data_out=%h", $time, fifo.count, full, empty, data_out);
 
     initial begin
